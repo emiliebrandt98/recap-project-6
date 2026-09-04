13a780dafcfc28d2ef9d38164f20471a8cce704d
Christopher <ch.welschoff@googlemail.com>
2026-09-04 15:05:52 +0200

    add layout

---
+++ components/Layout/index.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <header>Activity App</header>
+
+      <main>{children}</main>
+
+      <StyledFooter>
+        <Link href="/activityForm">
+          <Image width={24} height={24} src="/assets/circle-plus.png" />
+        </Link>
+      </StyledFooter>
+    </div>
+  );
+}
+
+const StyledFooter = styled.footer`
+  display: flex;
+  background-color: grey;
+  width: 100%;
+  padding: 10px;
+  justify-content: center;
+  position: fixed;
+  bottom: 0;
+  margin: 0 auto;
+
+  border: solid black 2px;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import ActivityList from "@/components/ActivityList";
+import ButtonToCreateActivity from "@/components/Navbar";
+import Layout from "@/components/Layout";
 
 export default function HomePage() {
   return (
     <div>
-      <ActivityList />
+      <Layout>
+        <ActivityList />
+      </Layout>
     </div>
   );
 }
