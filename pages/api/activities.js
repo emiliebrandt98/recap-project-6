a2a30081c63cff0a8e0a5a286777498dc874721e
Christopher <ch.welschoff@googlemail.com>
2026-09-07 10:21:32 +0200

    add backend code and timestamps

--- pages/api/activities.js
+++ pages/api/activities.js
@@ -5,9 +5,7 @@ export default async function handler(request, response) {
   try {
     await dbConnect();
   } catch (error) {
-    return response
-      .status(500)
-      .json({ error: "Database connection failed" });
+    return response.status(500).json({ error: "Database connection failed" });
   }
 
   if (request.method === "GET") {
@@ -15,9 +13,37 @@ export default async function handler(request, response) {
       const activities = await Activity.find().populate("categories");
       response.status(200).json(activities);
     } catch (error) {
-      response
-        .status(500)
-        .json({ error: "Error retrieving the activities" });
+      response.status(500).json({ error: "Error retrieving the activities" });
     }
   }
+
+  if (request.method === "POST") {
+    try {
+      const activitiesData = request.body;
+
+      const newActivity = await Activity.create(activitiesData);
+
+      const formattedActivity = newActivity.toObject();
+      formattedActivity.createdAt = new Date(
+        formattedActivity.createdAt
+      ).toLocaleString("de-DE", {
+        timeZone: "Europe/Berlin",
+      });
+      formattedActivity.updatedAt = new Date(
+        formattedActivity.updatedAt
+      ).toLocaleString("de-DE", {
+        timeZone: "Europe/Berlin",
+      });
+
+      return response
+        .status(201)
+        .json({ status: "Activity Created", activity: formattedActivity });
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ error: "Error creating the activity" });
+    }
+  }
+
+  response.status(405).json({ status: "Method not allowed" });
 }
