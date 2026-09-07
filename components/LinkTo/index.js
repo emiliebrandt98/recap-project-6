e61cdc2cad73aad059def668388b5bae034a1d9a
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-07 14:53:44 +0200

    add arrow icon to the back to list link

--- components/LinkTo/index.js
+++ components/LinkTo/index.js
@@ -1,5 +1,25 @@
+import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
+import styled from "styled-components";
 
 export default function LinkTo({ pathname }) {
-  return <StyledLink href={pathname}>Back to Activities List</StyledLink>;
+  return (
+    <StyledLink href={pathname}>
+      <ArrowLeft size={18} />
+      Back to Activities List
+    </StyledLink>
+  );
 }
+
+const StyledLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+
+  text-decoration: none;
+  color: #000;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
