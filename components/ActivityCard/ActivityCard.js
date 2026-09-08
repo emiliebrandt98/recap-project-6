4ba448cd51bada576f947eed4477e1cfc06efc9d
Christopher <ch.welschoff@googlemail.com>
2026-09-08 16:40:18 +0200

    delete un used import

--- components/ActivityCard/ActivityCard.js
+++ components/ActivityCard/ActivityCard.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
 
 export default function ActivityCard({ activities }) {
   return (
