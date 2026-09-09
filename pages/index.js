b08c3740ecb690bd24982863c7145875f482ac2f
Emilie Brandt <emiliebrandt@gmx.de>
2026-09-09 14:49:44 +0200

    solve merge conflicts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import ActivityList from "@/components/ActivityList";
+import { PrimaryButton } from "@/components/Button/Button";
 
 export default function HomePage({ activities, error, isLoading }) {
   if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error fetching data</p>;
+  if (error)
+    return (
+      <div>
+        <p>Error fetching data.</p>
+        <PrimaryButton
+          type="button"
+          buttonText={"Try again"}
+          onClick={() => mutate()}
+        />
+      </div>
+    );
   return (
     <div>
       <ActivityList activities={activities} />
