5f4929da4bde98de8a7e69fc4fb37045a6b7523c
Christopher <ch.welschoff@googlemail.com>
2026-09-14 12:41:18 +0200

    add searchbar on landingpage

--- components/features/ActivityList/ActivityList.js
+++ components/features/ActivityList/ActivityList.js
@@ -1,6 +1,24 @@
 import ActivityCard from "@/components/features/ActivityCard/ActivityCard.js";
+import { useState } from "react";
+import { Search } from "lucide-react";
+import styled from "styled-components";
 
-export default function ActivityList({ activities, activeCategories }) {
+export default function ActivityList({
+  activities,
+  isFavorite,
+  onToggle,
+  activeCategories,
+}) {
+  const [search, setSearch] = useState("");
+
+  const searchActivities = activities?.filter((activity) =>
+    activity.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const suggestions = searchActivities?.filter((activity) =>
+    activity.title.toLowerCase().startsWith(search.toLowerCase())
+  );
+
   if (!activities || activities.length === 0) {
     return <p>No activities found.</p>;
   }
@@ -13,14 +31,31 @@ export default function ActivityList({ activities, activeCategories }) {
     );
   }
 
-  const filteredActivities = activities.filter(matchesActiveCategories);
+  const filteredCategories = activities.filter(matchesActiveCategories);
 
-  if (filteredActivities.length === 0)
+  if (filteredCategories.length === 0) {
     return <p>No activities found for this category.</p>;
+  }
+
+  const activitiesToShow = search ? suggestions : filteredCategories;
 
   return (
-    <div>
-      {filteredActivities.map((activity) => {
+    <>
+      <label htmlFor="search">Search activities</label>
+
+      <SearchContainer>
+        <Search size={20} />
+
+        <input
+          id="search"
+          name="search"
+          placeholder="Search for Activities"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </SearchContainer>
+
+      {activitiesToShow.map((activity) => {
         return (
           <ActivityCard
             key={activity._id}
@@ -30,6 +65,21 @@ export default function ActivityList({ activities, activeCategories }) {
           />
         );
       })}
-    </div>
+    </>
   );
 }
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 8px 12px;
+  width: 300px;
+
+  input {
+    border: none;
+    outline: none;
+    width: 100%;
+  }
+`;
