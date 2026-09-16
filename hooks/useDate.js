9e3c162aad2b54f08ce6f94b28371c85ddf67136
Christopher <ch.welschoff@googlemail.com>
2026-09-16 15:52:34 +0200

    fix empty date field on create

--- hooks/useDate.js
+++ hooks/useDate.js
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function useDate(activity) {
-  const [startDate, setStartDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(null);
 
-  const [endDate, setEndDate] = useState(startDate);
+  const [endDate, setEndDate] = useState(null);
   useEffect(() => {
     if (activity?.startDate) {
       setStartDate(new Date(activity.startDate));
