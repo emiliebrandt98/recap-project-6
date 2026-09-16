1917b3005014e6450ecd1b7acd878f0b895f5c91
Christopher <ch.welschoff@googlemail.com>
2026-09-16 17:20:54 +0200

    restore working date hook

--- hooks/useDate.js
+++ hooks/useDate.js
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function useDate(activity) {
-  const [startDate, setStartDate] = useState(null);
+  const [startDate, setStartDate] = useState(new Date());
 
-  const [endDate, setEndDate] = useState(null);
+  const [endDate, setEndDate] = useState(startDate);
   useEffect(() => {
     if (activity?.startDate) {
       setStartDate(new Date(activity.startDate));
