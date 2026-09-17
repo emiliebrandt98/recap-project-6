d6b8767f2739a61a5abfd58c5266eedeb2026536
Christopher <ch.welschoff@googlemail.com>
2026-09-17 09:41:43 +0200

    delete double mutate

--- pages/activities/[id]/updateActivity.js
+++ pages/activities/[id]/updateActivity.js
@@ -21,7 +21,6 @@ export default function UpdateActivity({ activities }) {
       return;
     }
 
-    mutate("/api/activities");
     mutate(`/api/activities/${id}`);
     mutate("/api/activities");
     toast.success("Activity successfully updated!");
